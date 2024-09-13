3e5d6f79a3347d5525fd5b7a5f0342bcb75f8d41
Elias Lorandi <lorandi.elias@gmail.com>
2024-09-13 16:07:14 -0300

    Rotas

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,21 +1,103 @@
-import {createBrowserRouter} from "react-router-dom";
-import Login from "./views/Login";
-import Signup from "./views/Signup";
-import Users from "./views/Users";
+import { createBrowserRouter, Navigate, Routes, useRoutes } from "react-router-dom";
+import Login from "./views/Login.jsx";
+import Signup from "./views/Signup.jsx";
+import Users from "./views/Users.jsx";
+import NotFound from "./views/NotFound.jsx";
+import Dashboard from "./views/Dashboard.jsx";
+import DefaultLayout from "./components/DefaultLayout.jsx";
+import GuestLayout from "./components/GuestLayout.jsx";
 
 const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Navigate to="/users" />
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            },
+            {
+                path: '/users',
+                element: <Users />
+            },
+        ]
+    },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+        ]
+    },
     {
         path: '/login',
         element: <Login />
     },
     {
         path: '/signup',
-        element: <signup />
+        element: <Signup />
     },
     {
         path: '/users',
-        element: <users />
+        element: <Users />
+    },
+    {
+        path: '*',
+        element: <NotFound />
     },
 ])
 
-export default Router;
+
+//novo
+// const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <DefaultLayout />,
+//         children: [
+//             {
+//                 path: '/',
+//                 element: <Navigate to="/users" />
+//             },
+//             {
+//                 path: '/dashboard',
+//                 element: <Dashboard />
+//             },
+//             {
+//                 path: '/users',
+//                 element: <Users />
+//             },
+//         ],
+//     },
+//     {
+//         path: '/',
+//         element: <GuestLayout />,
+//         children: [
+//             {
+//                 path: '/login',
+//                 element: <Login />
+//             },
+//             {
+//                 path: '/signup',
+//                 element: <Signup />
+//             },
+//         ],
+//     },
+//     {
+//         path: '*',
+//         element: <NotFound />
+//     },
+// ]);
+
+
+export default router;
