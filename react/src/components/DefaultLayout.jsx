190391a8e406d0e1ce7f9ef2bfa188d2ffefe61a
Elias Lorandi <lorandi.elias@gmail.com>
2024-09-13 16:26:39 -0300

    Verificações e proteção de rotas

---
+++ react/src/components/DefaultLayout.jsx
@@ -0,0 +1,17 @@
+import { Navigate, Outlet } from "react-router-dom"
+import { useStateContext } from "../contexts/ContextProvider.jsx"
+
+export default function DefaultLayout() {
+    const {user, token} = useStateContext()
+
+    if (!token) {
+        return <Navigate to="/login" />
+    }
+
+    return (
+        <div>
+            Default
+            <Outlet />
+        </div>
+    )
+}
