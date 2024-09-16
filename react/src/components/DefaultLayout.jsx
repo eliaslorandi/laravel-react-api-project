b97dc1ecc82f52be49959e9ab7ba5f51cdd23e20
Elias Lorandi <lorandi.elias@gmail.com>
2024-09-16 19:31:11 -0300

    Css, default layout e login

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,17 +1,35 @@
-import { Navigate, Outlet } from "react-router-dom"
+import { Link, Navigate, Outlet } from "react-router-dom"
 import { useStateContext } from "../contexts/ContextProvider.jsx"
 
 export default function DefaultLayout() {
-    const {user, token} = useStateContext()
+    const { user, token } = useStateContext()
 
     if (!token) {
         return <Navigate to="/login" />
     }
 
+    const onLogout = () => {}
+
     return (
-        <div>
-            Default
-            <Outlet />
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">Users</Link>
+            </aside>
+            <div className="content">
+                <header>
+                    <div>
+                        header
+                    </div>
+                    <div>
+                        {user.name}
+                        <a href="#" onClick={onLogout} className="btn-logout">Logout</a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
         </div>
     )
 }
--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -8,8 +8,10 @@ const StateContext = createContext({
 })
 
 export const ContextProvider = ({children}) => {
-    const [user ,setUser] = useState({});
-    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'))
+    const [user ,setUser] = useState({
+        name: 'jhon'
+    });
+    const [token, _setToken] = useState(null); //localStorage.getItem('ACCESS_TOKEN')
 
     const setToken = (token) => {
         _setToken(token)
