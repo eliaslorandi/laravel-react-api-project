185fd6b8902e165c8ead5693989a111bbeb704b9
Elias Lorandi <lorandi.elias@gmail.com>
2024-09-19 11:18:47 -0300

    Implementação de login, logout e signup na API

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SignupRequest;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request) {
+        $credentials = $request->validated();
+        if(!Auth::attempt($credentials)){
+            return response([
+                'message' => 'Provided email or password is incorrect'
+            ]);
+        }
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response([ //irá para o data do clientside em login
+            'user' => $user,
+            'token' => $token,
+        ]);
+
+    }
+
+    public function signup(SignupRequest $request) {
+        $data = $request->validated();
+        /** @var \App\Models\User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([ //irá para o data do clientside em signup.jsx
+            'user' => $user,
+            'token' => $token,
+        ]);
+
+        //posso compactar para:
+        //return response(compact('user', 'token'));
+    }
+
+    public function logout(Request $request) {
+        /** @var \App\Models\User $user */
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response('', 204);
+    }
+}
