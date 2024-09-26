eede2ec90f2628f35d25a3b80a555c111e26a825
Elias Lorandi <lorandi.elias@gmail.com>
2024-09-26 17:56:50 -0300

    CRUD de usuários em API

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,5 +1,27 @@
-export default function Users(){
-    return(
+import { useEffect, useState } from "react"
+
+export default function Users() {
+    const [users, serUsers] = useState([]);
+    const [loading, setLoading] = useState(false); //carregar usuarios
+
+    useEffect(() => {
+        getUsers();
+    }, [])  //array vazio como segundo argumento
+
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+            .then(({ data }) => {
+                setLoading(false)
+                console.log(data);
+            })
+            .catch(() => {
+                setLoading(false)
+            })
+    }
+
+
+    return (
         <div>
             Users
         </div>
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,17 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::apiResource('/users', [UserController::class]);
 });
 
 Route::post('/signup', [AuthController::class, 'signup']);
