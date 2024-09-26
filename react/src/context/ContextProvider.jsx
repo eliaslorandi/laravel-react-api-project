89089200a69a89741563faf325b01f63f326d479
Elias Lorandi <lorandi.elias@gmail.com>
2024-09-26 10:20:41 -0300

    IMplementação de logout

--- react/src/context/ContextProvider.jsx
+++ react/src/context/ContextProvider.jsx
@@ -9,7 +9,6 @@ const StateContext = createContext({
 
 export const ContextProvider = ({children}) => {
     const [user ,setUser] = useState({
-        name: 'jhon'
     });
     const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN')); //localStorage.getItem('ACCESS_TOKEN')
 
