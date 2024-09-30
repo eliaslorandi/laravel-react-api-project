7f026609fe74edd5cd63dd2b1efa98c0b313c33f
Elias Lorandi <lorandi.elias@gmail.com>
2024-09-30 10:45:40 -0300

    Correção em rota

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,9 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
     Route::post('/logout', [AuthController::class, 'logout']);
-    
-    Route::apiResource('/users', [UserController::class]);
+    Route::apiResource('/users', UserController::class);
 });
 
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/login', [AuthController::class, 'login']);
+
